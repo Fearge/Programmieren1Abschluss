b3f3c86366eba8708bea438aca55ee1581eadca1
wasgehtey <linus.ollmann@haw-hamburg.de>
2024-10-11 15:25:25 +0200

    Hinzugefügt: Start Bildschirm

--- src/screen.py
+++ src/screen.py
@@ -81,3 +81,46 @@ class Screen:
 			if hits:
 				for hit in hits:
 					collide_with_obstacles(enemy, hit)
+
+class StartScreen:
+    def __init__(self, game):
+        self.game = game
+        self.font = pg.font.Font(None, 74)  # Verwende eine Standard-Schriftart
+        self.button_font = pg.font.Font(None, 48)
+        self.button_color = (255, 255, 255)  # Weiß
+        self.hover_color = (200, 200, 200)  # Hellgrau
+        self.start_button_rect = pg.Rect((WIDTH // 2 - 75, HEIGHT // 2), (150, 50))  # Button-Rechteck
+
+    def draw(self):
+        # Hintergrundeinstellung (z. B. schwarz)
+        self.game.surface.fill((0, 0, 0))
+
+        # Titeltext zeichnen
+        title_surface = self.font.render("Programmieren 1", True, (255, 255, 255))
+        title_rect = title_surface.get_rect(center=(WIDTH // 2, HEIGHT // 4))
+        self.game.surface.blit(title_surface, title_rect)
+
+        # Startbutton zeichnen
+        button_color = self.hover_color if self.start_button_rect.collidepoint(pg.mouse.get_pos()) else self.button_color
+        pg.draw.rect(self.game.surface, button_color, self.start_button_rect)
+
+        button_text_surface = self.button_font.render("Start", True, (0, 0, 0))  # Textfarbe schwarz
+        button_text_rect = button_text_surface.get_rect(center=self.start_button_rect.center)
+        self.game.surface.blit(button_text_surface, button_text_rect)
+
+        pg.display.flip()  # Aktualisiere den Bildschirm
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:  # Linke Maustaste
+                if self.start_button_rect.collidepoint(pg.mouse.get_pos()):  # Überprüfen, ob der Button geklickt wurde
+                    self.game.set_screen(Screen(self.game))  # Zum Hauptspiel wechseln
+
+    def run(self):
+        while True:
+            self.handle_events()
+            self.draw()
